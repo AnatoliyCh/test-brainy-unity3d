797bec01fe367c0700b88fe3a0f97de4b7c0e701
Anatoly <googlsocial97@gmail.com>
2020-03-16 02:15:55 +0700

    add GameController

---
+++ Assets/Source/1_Business/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //player
+    //player
+    //generator
+    //path
+
+    private void Awake()
+    {
+        LevelGeneration();
+        PathGeneration();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void LevelGeneration()
+    {
+
+    }
+    private void PathGeneration()
+    {
+
+    }
+
+    public void GameReset()
+    {
+        
+    }
+}
